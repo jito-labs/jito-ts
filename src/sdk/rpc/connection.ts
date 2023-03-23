76f7492005a205433cf4d0f0f8c915a41274a04a
futurekarol <29566494+futurekarol@users.noreply.github.com>
2023-03-23 11:39:17 -0400

    add permalinks to sources in web3.js

--- src/sdk/rpc/connection.ts
+++ src/sdk/rpc/connection.ts
@@ -1,3 +1,6 @@
+// most stuff copied from @solana/web3.js 1.74.0 - see direct references in the comment of each snippet
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   Commitment,
@@ -92,6 +95,7 @@ export type SimulatedBundleResponse = {
   transactionResults: SimulatedBundleTransactionResult[];
 };
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L387
 function createRpcResult<T, U>(result: Struct<T, U>) {
   return union([
     pick({
@@ -111,8 +115,10 @@ function createRpcResult<T, U>(result: Struct<T, U>) {
   ]);
 }
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L406
 const UnknownRpcResult = createRpcResult(unknown());
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L411
 function jsonRpcResult<T, U>(schema: Struct<T, U>) {
   return coerce(createRpcResult(schema), UnknownRpcResult, value => {
     if ('error' in value) {
@@ -126,6 +132,7 @@ function jsonRpcResult<T, U>(schema: Struct<T, U>) {
   });
 }
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L427
 function jsonRpcResultAndContext<T, U>(value: Struct<T, U>) {
   return jsonRpcResult(
     pick({
@@ -192,6 +199,7 @@ const SimulatedBundleResponseStruct = jsonRpcResultAndContext(
   })
 );
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L1489
 function createRpcClient(
   url: string,
   httpHeaders?: HttpHeaders,
@@ -324,8 +332,10 @@ function createRpcClient(
   return clientBrowser;
 }
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L210
 type RpcRequest = (methodName: string, args: Array<any>) => Promise<any>;
 
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L1620
 function createRpcRequest(client: RpcClient): RpcRequest {
   return (method, args) => {
     return new Promise((resolve, reject) => {
@@ -356,6 +366,8 @@ export class JitoRpcConnection extends Connection {
   /** @internal */ _rpcClient: RpcClient;
   /** @internal */ _rpcRequest: RpcRequest;
 
+  // copied from here but removed unused fields
+  // https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/connection.ts#L3060
   constructor(
     endpoint: string,
     commitmentOrConfig?: Commitment | ConnectionConfig
--- src/sdk/rpc/fetch-impl.ts
+++ src/sdk/rpc/fetch-impl.ts
@@ -1,3 +1,5 @@
+// copied from @solana/web3.js 1.74.0
+// https://github.com/solana-labs/solana-web3.js/blob/3cd0cfd91a7fe08b9c67ac6f0d0c31c0c2ae8157/packages/library-legacy/src/fetch-impl.ts
 import * as nodeFetch from 'node-fetch';
 
 export * from 'node-fetch';
