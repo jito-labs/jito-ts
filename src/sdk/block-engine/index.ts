1419b5b5aa355fbeb40de824da50e13207568d64
mdr0id <marshall@jito.wtf>
2025-11-01 10:03:48 -0600

    update paths for graceful imports

--- src/sdk/block-engine/index.ts
+++ src/sdk/block-engine/index.ts
@@ -1,2 +1,68 @@
+export { SearcherClient, SearcherClientError } from './searcher';
+export { AuthProvider as BlockEngineAuthProvider, Jwt, authInterceptor, AuthRefreshError } from './auth';
+export { Bundle } from './types';
+export * from './utils';
+
+export {
+  Role,
+  GenerateAuthChallengeRequest,
+  GenerateAuthChallengeResponse,
+  GenerateAuthTokensRequest,
+  GenerateAuthTokensResponse,
+  RefreshAccessTokenRequest,
+  RefreshAccessTokenResponse,
+  Token,
+} from '../../gen/block-engine/auth';
+
+export {
+  BundleUuid,
+  BundleResult,
+  Accepted,
+  Rejected,
+  Finalized,
+  Processed,
+  Dropped,
+  DroppedReason,
+  StateAuctionBidRejected,
+  WinningBatchBidRejected,
+  SimulationFailure,
+  InternalError,
+  DroppedBundle,
+} from '../../gen/block-engine/bundle';
+
+export {
+  SendBundleRequest,
+  SendBundleResponse,
+  NextScheduledLeaderRequest,
+  NextScheduledLeaderResponse,
+  ConnectedLeadersRequest,
+  ConnectedLeadersResponse,
+  ConnectedLeadersRegionedRequest,
+  ConnectedLeadersRegionedResponse,
+  GetTipAccountsRequest,
+  GetTipAccountsResponse,
+  SubscribeBundleResultsRequest,
+  GetRegionsRequest,
+  GetRegionsResponse,
+  SlotList,
+} from '../../gen/block-engine/searcher';
+
+export {
+  Header,
+  Heartbeat,
+  Socket,
+} from '../../gen/block-engine/shared';
+
+export {
+  Packet,
+  PacketBatch,
+  Meta,
+  PacketFlags,
+} from '../../gen/block-engine/packet';
+
+export { AuthServiceClient } from '../../gen/block-engine/auth';
+export { SearcherServiceClient } from '../../gen/block-engine/searcher';
+
+// Dont break old namespace exports
 export * as bundle from './types';
 export * as searcher from './searcher';
